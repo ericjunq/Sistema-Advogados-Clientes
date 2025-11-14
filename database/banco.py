81726e510ef51cbb90ad7a63efd0b29b61236212
ericjunq <junqueiraeric576@gmail.com>
2025-11-13 22:22:14 -0300

    Update banco.py

--- database/banco.py
+++ database/banco.py
@@ -1,5 +1,6 @@
 import sqlite3
 from models.cliente import Cliente
+from models.advogado import Advogado
 
 DB_PATH = 'banco.db'
 
@@ -14,7 +15,6 @@ def criar_tabela_cliente():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
-        senha TEXT NOT NULL,
         username TEXT UNIQUE NOT NULL,
         cpf TEXT UNIQUE NOT NULL,
         telefone TEXT UNIQUE NOT NULL,
@@ -30,5 +30,51 @@ def inserir_cliente(cliente):
 
 def atualizar_cliente(cliente):
     conn = conectar()
-    cursor = conn.execute()
-    cursor.execute('UPDATE clientes ')
+    cursor = conn.cursor()
+    cursor.execute('''UPDATE clientes
+    SET nome = ?,
+        email = ?,
+        username = ?,
+        cpf = ?,
+        telefone = ?
+    WHERE id = ?
+         ''', (cliente.nome, cliente.email, cliente.username, cliente.cpf, cliente.telefone))
+    
+    conn.commit()
+    conn.close()
+    
+
+def criar_tabela_advogado():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS advogados(
+            id PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL, 
+            email TEXT UNIQUE NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            cpf TEXT UNIQUE NOT NULL,     
+            telefone TEXT UNIQUE NOT NULL,
+            oab TEXT UNIQUE NOT NULL,
+            status INTEGER DEFAULT 1)
+    ''',)
+
+def inserir_advogado(advogado):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO advogados (nome, email, username, cpf, telefone, oab, status)', (advogado.nome, advogado.email, advogado.username, advogado.cpf, advogado.telefone, advogado.oab, advogado.status))
+    conn.commit()
+    conn.close()
+
+def atualizar_advogado(advogado):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(''' UPDATE advogados
+        SET nome = ?,
+            email = ?,
+            username = ?,
+            cpf = ?,
+            telefone = ?,
+            oab = ?
+        WHERE id = ?
+    ''', (advogado.nome, advogado.email, advogado.username, advogado.cpf, advogado.telefone, advogado.oab))
