5de0e6168aa49da0efbb4d364a061665cba27506
ericjunq <junqueiraeric576@gmail.com>
2025-11-13 22:20:42 -0300

    Update cliente.py

--- models/cliente.py
+++ models/cliente.py
@@ -1,5 +1,6 @@
 class Cliente:
-    def __init__(self, nome, email, senha, username, cpf, telefone, status = 1):
+    def __init__(self, id, nome, email, senha, username, cpf, telefone, status = 1):
+        self.id = id
         self.nome = nome
         self.email = email
         self.senha = senha
@@ -17,5 +18,19 @@ class Cliente:
         cpf = input('Digite seu CPF: ')
         telefone = input('Digite seu número de telefone: ')
 
-        cliente = Cliente(nome, email, senha, username, cpf, telefone)
-        return cls(cliente)
+        return cls(nome, email, senha, username, cpf, telefone)
+       
+    def atualizar_cliente(self):
+        novo_nome = input(f'Digite o novo nome ({self.nome}):') or self.nome
+        novo_email = input(f'Digite o novo email ({self.email}): ') or self.email
+        nova_senha = input(f'Digite a nova senha ({self.senha}): ') or self.senha
+        novo_username = input(f'Digite o novo username ({self.username}): ') or self.username
+        novo_cpf = input(f'Digite o novo CPF ({self.cpf}): ') or self.cpf
+        novo_telefone = input(f'Digite o novo número de telefone ({self.telefone}): ') or self.telefone
+
+        self.nome = novo_nome
+        self.email = novo_email
+        self.senha = nova_senha
+        self.username = novo_username
+        self.cpf = novo_cpf
+        self.telefone = novo_telefone
